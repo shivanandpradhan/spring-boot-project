3013284569e6535aee19f8bd24e3fd0d162bf1b3
shivanandpradhan <shivanandpradhan0@gmail.com>
2022-09-16 14:24:10 +0530

    wir project

---
+++ wir/src/main/java/com/sapient/wir/WirApplication.java
@@ -0,0 +1,13 @@
+package com.sapient.wir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WirApplication.class, args);
+	}
+
+}
---
+++ wir/src/test/java/com/sapient/wir/WirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapient.wir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
